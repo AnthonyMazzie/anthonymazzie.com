45003c59216b1ef928b258902345797cc097e615
ajmazzie <ajmazzie@iastate.edu>
2021-12-06 17:47:25 -0600

    resume res

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -6,7 +6,7 @@ const ResumePage = () => {
       <iframe
         src="https://drive.google.com/file/d/1Htwq1FNKIa9NsKdzykwRPWk9lzrpClaM/preview"
         width="100%"
-        height="1200px"
+        height="800pt"
         allow="autoplay"
         title="AnthonyMazzieResume_2021"
       ></iframe>

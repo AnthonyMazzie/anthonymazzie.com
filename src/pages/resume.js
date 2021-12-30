b8c3e0e192420d8045e8fb09b55c38f338a91d39
ajmazzie <ajmazzie@iastate.edu>
2021-12-30 08:09:12 -0600

    update resume link

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -4,7 +4,8 @@ const ResumePage = () => {
   return (
     <div>
       <iframe
-        src="https://drive.google.com/file/d/1Htwq1FNKIa9NsKdzykwRPWk9lzrpClaM/preview"
+        // src="https://drive.google.com/file/d/1Htwq1FNKIa9NsKdzykwRPWk9lzrpClaM/preview"
+        src="https://drive.google.com/file/d/1t7RLzF7P0wJ9p6O1hhFSDTzM2rxwvC9R/preview"
         width="100%"
         height="800pt"
         allow="autoplay"

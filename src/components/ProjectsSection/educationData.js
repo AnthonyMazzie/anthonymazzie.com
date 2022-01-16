ea05a1b1430aee6ab3587b4cdb944db63031ccce
ajmazzie <ajmazzie@iastate.edu>
2022-01-15 20:08:05 -0600

    accordians

---
+++ src/components/ProjectsSection/educationData.js
@@ -0,0 +1,102 @@
+const engineeringCourses = [
+  {
+    id: 1,
+    title: "Digital Logic",
+    grade: "A-",
+    description:
+      "CPR E 281: Number systems and representation. Boolean algebra and logic minimization. Combinational and sequential logic design. Arithmetic circuits and finite state machines. Use of programmable logic devices. Introduction to computer-aided schematic capture systems, simulation tools, and hardware description languages. Design of simple digital systems. *Source: Iowa State University Catalog",
+  },
+  {
+    id: 2,
+    title: "Problem Solving in Software Engineering",
+    grade: "A",
+    description:
+      "SE 185: Introduction to software engineering and computer programming. Systematic thinking process for problem solving in the context of software engineering. Group problem solving. Solving software engineering problems and presenting solutions through computer programs, written documents and oral presentations. Introduction to principles of programming, software design, and extensive practice in design, writing, running, debugging, and reasoning about programs. *Source: Iowa State University Catalog",
+  },
+  {
+    id: 3,
+    title: "Object-Oriented Programming",
+    grade: "C+",
+    description:
+      "COM S 227: Computer programming using objects as the mechanism for modularity, abstraction, and code reuse. Instance variables, methods, and encapsulation. Review of control structures for conditionals and iteration. Developing algorithms on strings, arrays, and lists. Recursion, searching, and sorting. Text parsing and file I/O. Interfaces, inheritance, polymorphism, and abstract classes. Exception handling. Tools for unit testing and debugging. Emphasis on a disciplined approach to specification, code development, and testing. *Source: Iowa State University Catalog",
+  },
+  {
+    id: 4,
+    title: "Data Structures and Algorithms",
+    grade: "B",
+    description:
+      "COM S 228: An object-oriented approach to data structures and algorithms. Object-oriented analysis, design, and programming, with emphasis on data abstraction, inheritance and subtype polymorphism, and generics. Abstract data type specification and correctness. Collections including lists, stacks, queues, trees, heaps, maps, hash tables, and graphs. Big-O notation and algorithm analysis. Searching and sorting. Graph search and shortest path algorithms. Emphasis on object-oriented design, writing and documenting medium-sized programs. This course is designed for majors. *Source: Iowa State University Catalog",
+  },
+  {
+    id: 5,
+    title: "Embedded Systems 1",
+    grade: "A",
+    description:
+      "CPR E 288: Embedded C programming. Interrupt handling. Memory mapped I/O in the context of an application. Elementary embedded design flow/methodology. Timers, scheduling, resource allocation, optimization, state machine based controllers, real time constraints within the context of an application. Applications laboratory exercises with embedded devices. *Source: Iowa State University Catalog",
+  },
+  {
+    id: 6,
+    title: "Principles of Programming Languages",
+    grade: "B",
+    description:
+      "COM S 342: Study of concepts in programming languages, especially functional programming concepts. Overview of major programming paradigms, their relationship, and tradeoffs among paradigms enabling sound choices of programming language for application-specific development. Programming projects. *Source: Iowa State University Catalog",
+  },
+  {
+    id: 7,
+    title: "Software Development Practices",
+    grade: "B+",
+    description:
+      "COM S 309: A practical introduction to methods for managing software development. Process models, requirements analysis, structured and object-oriented design, coding, testing, maintenance, cost and schedule estimation, metrics. Programming projects. *Source: Iowa State University Catalog",
+  },
+  {
+    id: 8,
+    title: "Construction of User Interfaces",
+    grade: "A",
+    description:
+      "COM S 319: Overview of user interface design. Evaluation and testing of user interfaces. Review of principles of object orientation, object oriented design and analysis using UML in the context of user interface design. Design of windows, menus and commands. Developing Web and Windows-based user-interfaces. Event-driven programming. Introduction to Frameworks and APIs for the construction of user interfaces. *Source: Iowa State University Catalog",
+  },
+  {
+    id: 9,
+    title:
+      "Introduction to Computer Architecture and Machine-Level Programming",
+    grade: "B-",
+    description:
+      "COM S 321: Introduction to computer architecture and organization. Emphasis on evaluation of performance, instruction set architecture, datapath and control, memory-hierarchy design, and pipelining. Assembly language programming. *Source: Iowa State University Catalog",
+  },
+  {
+    id: 10,
+    title: "Database Management Systems",
+    grade: "C",
+    description:
+      "COM S 363: Relational, object-oriented, semistructured and query languages. SQL, XML, and NO-SQL. Database design using entity-relationship model, data dependencies, and relational database design. Application development in SQL-like languages and general purpose host languages with application program interfaces and a commonly used Object Relational Mapping framework. Web application development. Programming Projects. *Source: Iowa State University Catalog",
+  },
+  {
+    id: 11,
+    title: "Advanced Programming Techniques",
+    grade: "In Progress",
+    description:
+      "COM S 327: Object-oriented programming experience using a language suitable for exploring advanced topics in programming. Topics include memory management, parameter passing, inheritance, compiling, debugging, and maintaining programs. Significant programming projects. *Source: Iowa State University Catalog",
+  },
+  {
+    id: 12,
+    title: "Operating Systems: Principles and Practice",
+    grade: "In Progress",
+    description:
+      "CPR E 308: Operating system concepts, processes, threads, synchronization between threads, process and thread scheduling, deadlocks, memory management, file systems, I/O systems,security, Linux-based lab experiments. *Source: Iowa State University Catalog",
+  },
+  {
+    id: 13,
+    title: "Introduction to Software Testing",
+    grade: "In Progress",
+    description:
+      "SE 317: Basic principles and techniques for software testing. Test requirements and management. Test design techniques, evaluation metrics, model-based testing, unit testing, system and integration testing. Software testing tools and programming projects. *Source: Iowa State University Catalog",
+  },
+  {
+    id: 14,
+    title: "Software Architecture and Design",
+    grade: "In Progress",
+    description:
+      "SE 339: Modeling and design of software at the architectural level. Architectural styles. Basics of model-driven architecture. Object-oriented design and analysis. Iterative development and unified process. Design patterns. Design by contract. Component based design. Product families. Measurement theory and appropriate use of metrics in design. Designing for qualities such as performance, safety, security, reliability, reusability, etc. Analysis and evaluation of software architectures. Introduction to architecture definition languages. Basics of software evolution, reengineering, and reverse engineering. Case studies. Introduction to distributed system software. *Source: Iowa State University Catalog",
+  },
+];
+export default engineeringCourses;
--- src/components/ProjectsSection/index.js
+++ src/components/ProjectsSection/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import ExternLink from "./gitLink";
 import MazzieCafe from "./mazzieCafeLink";
 import {
@@ -16,6 +16,14 @@ import {
   LinkWrap,
 } from "./ProjectElements";
 
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import Typography from "@mui/material/Typography";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import Stack from "@mui/material/Stack";
+import EducationData from "./educationData";
+
 import Aos from "aos";
 import "aos/dist/aos.css";
 
@@ -32,6 +40,8 @@ const ProjectsSection = ({
   img,
   alt,
 }) => {
+  const [educationItems] = useState(EducationData);
+
   useEffect(() => {
     Aos.init({ duration: 3000 });
   }, []);
@@ -47,142 +57,36 @@ const ProjectsSection = ({
                   <Heading lightText={lightText}>{headLine}</Heading>
                   <SubTitle darkText={darkText}>{description1}</SubTitle>
                   <SubTitle darkText={darkText}>{description2}</SubTitle>
-                  <ul class="list-group">
-                    <li
-                      class="list-group-item d-flex justify-content-between align-items-center"
-                      data-bs-toggle="tooltip"
-                      data-bs-placement="top"
-                      title="From the Iowa State Catalog: CPRE281 - Number systems and representation. Boolean algebra and logic minimization. Combinational and sequential logic design. Arithmetic circuits and finite state machines. Use of programmable logic devices. Introduction to computer-aided schematic capture systems, simulation tools, and hardware description languages. Design of simple digital systems.."
-                    >
-                      Digital Logic
-                      <span class="badge bg-primary rounded-pill">A-</span>
-                    </li>
-                    <li
-                      class="list-group-item d-flex justify-content-between align-items-center"
-                      data-bs-toggle="tooltip"
-                      data-bs-placement="top"
-                      title="From the Iowa State Catalog: SE185 - Introduction to software engineering and computer programming. Systematic thinking process for problem solving in the context of software engineering. Group problem solving. Solving software engineering problems and presenting solutions through computer programs, written documents and oral presentations. Introduction to principles of programming, software design, and extensive practice in design, writing, running, debugging, and reasoning about programs."
-                    >
-                      Problem Solving in Software Engineering
-                      <span class="badge bg-primary rounded-pill">A</span>
-                    </li>
-                    <li
-                      class="list-group-item d-flex justify-content-between align-items-center"
-                      data-bs-toggle="tooltip"
-                      data-bs-placement="top"
-                      title="From the Iowa State Catalog: COMS227 - Computer programming using objects as the mechanism for modularity, abstraction, and code reuse. Instance variables, methods, and encapsulation. Review of control structures for conditionals and iteration. Developing algorithms on strings, arrays, and lists. Recursion, searching, and sorting. Text parsing and file I/O. Interfaces, inheritance, polymorphism, and abstract classes. Exception handling. Tools for unit testing and debugging. Emphasis on a disciplined approach to specification, code development, and testing."
-                    >
-                      Object-Oriented Programming
-                      <span class="badge bg-primary rounded-pill">C+</span>
-                    </li>
-                    <li
-                      class="list-group-item d-flex justify-content-between align-items-center"
-                      data-bs-toggle="tooltip"
-                      data-bs-placement="top"
-                      title="From the Iowa State Catalog: COMS228 - An object-oriented approach to data structures and algorithms. Object-oriented analysis, design, and programming, with emphasis on data abstraction, inheritance and subtype polymorphism, and generics. Abstract data type specification and correctness. Collections including lists, stacks, queues, trees, heaps, maps, hash tables, and graphs. Big-O notation and algorithm analysis. Searching and sorting. Graph search and shortest path algorithms. Emphasis on object-oriented design, writing and documenting medium-sized programs. This course is designed for majors."
-                    >
-                      Data Structures and Algorithms
-                      <span class="badge bg-primary rounded-pill">B</span>
-                    </li>
-                    <li
-                      class="list-group-item d-flex justify-content-between align-items-center"
-                      data-bs-toggle="tooltip"
-                      data-bs-placement="top"
-                      title="From the Iowa State Catalog: CPRE288 - Embedded C programming. Interrupt handling. Memory mapped I/O in the context of an application. Elementary embedded design flow/methodology. Timers, scheduling, resource allocation, optimization, state machine based controllers, real time constraints within the context of an application. Applications laboratory exercises with embedded devices."
-                    >
-                      Embedded Systems 1
-                      <span class="badge bg-primary rounded-pill">A</span>
-                    </li>
-                    <li
-                      class="list-group-item d-flex justify-content-between align-items-center"
-                      data-bs-toggle="tooltip"
-                      data-bs-placement="top"
-                      title="From the Iowa State Catalog: COMS342 - Study of concepts in programming languages, especially functional programming concepts. Overview of major programming paradigms, their relationship, and tradeoffs among paradigms enabling sound choices of programming language for application-specific development. Programming projects."
-                    >
-                      Principles of Programming Languages
-                      <span class="badge bg-primary rounded-pill">B</span>
-                    </li>
-                    <li
-                      class="list-group-item d-flex justify-content-between align-items-center"
-                      data-bs-toggle="tooltip"
-                      data-bs-placement="top"
-                      title="From the Iowa State Catalog: COMS309 - A practical introduction to methods for managing software development. Process models, requirements analysis, structured and object-oriented design, coding, testing, maintenance, cost and schedule estimation, metrics. Programming projects."
-                    >
-                      Software Development Practices
-                      <span class="badge bg-primary rounded-pill">B+</span>
-                    </li>
-                    <li
-                      class="list-group-item d-flex justify-content-between align-items-center"
-                      data-bs-toggle="tooltip"
-                      data-bs-placement="top"
-                      title="From the Iowa State Catalog: COMS319 - Overview of user interface design. Evaluation and testing of user interfaces. Review of principles of object orientation, object oriented design and analysis using UML in the context of user interface design. Design of windows, menus and commands. Developing Web and Windows-based user-interfaces. Event-driven programming. Introduction to Frameworks and APIs for the construction of user interfaces."
-                    >
-                      Construction of User Interfaces
-                      <span class="badge bg-primary rounded-pill">A</span>
-                    </li>
-                    <li
-                      class="list-group-item d-flex justify-content-between align-items-center"
-                      data-bs-toggle="tooltip"
-                      data-bs-placement="top"
-                      title="From the Iowa State Catalog: COMS321 - Introduction to computer architecture and organization. Emphasis on evaluation of performance, instruction set architecture, datapath and control, memory-hierarchy design, and pipelining. Assembly language programming."
-                    >
-                      Introduction to Computer Architecture and Machine-Level
-                      Programming
-                      <span class="badge bg-primary rounded-pill">B-</span>
-                    </li>
-                    <li
-                      class="list-group-item d-flex justify-content-between align-items-center"
-                      data-bs-toggle="tooltip"
-                      data-bs-placement="top"
-                      title="From the Iowa State Catalog: COMS363 - Relational, object-oriented, semistructured and query languages. SQL, XML, and NO-SQL. Database design using entity-relationship model, data dependencies, and relational database design. Application development in SQL-like languages and general purpose host languages with application program interfaces and a commonly used Object Relational Mapping framework. Web application development. Programming Projects."
-                    >
-                      Database Management Systems
-                      <span class="badge bg-primary rounded-pill">C</span>
-                    </li>
-                    <li
-                      class="list-group-item d-flex justify-content-between align-items-center"
-                      data-bs-toggle="tooltip"
-                      data-bs-placement="top"
-                      title="From the Iowa State Catalog: COMS327 - Object-oriented programming experience using a language suitable for exploring advanced topics in programming. Topics include memory management, parameter passing, inheritance, compiling, debugging, and maintaining programs. Significant programming projects."
-                    >
-                      Advanced Programming Techniques
-                      <span class="badge bg-primary rounded-pill">
-                        IN PROGRESS
-                      </span>
-                    </li>
-                    <li
-                      class="list-group-item d-flex justify-content-between align-items-center"
-                      data-bs-toggle="tooltip"
-                      data-bs-placement="top"
-                      title="From the Iowa State Catalog: CPRE308 - Operating system concepts, processes, threads, synchronization between threads, process and thread scheduling, deadlocks, memory management, file systems, I/O systems,security, Linux-based lab experiments."
-                    >
-                      Operating Systems: Principles and Practice
-                      <span class="badge bg-primary rounded-pill">
-                        IN PROGRESS
-                      </span>
-                    </li>
-                    <li
-                      class="list-group-item d-flex justify-content-between align-items-center"
-                      data-bs-toggle="tooltip"
-                      data-bs-placement="top"
-                      title="From the Iowa State Catalog: SE317 - Basic principles and techniques for software testing. Test requirements and management. Test design techniques, evaluation metrics, model-based testing, unit testing, system and integration testing. Software testing tools and programming projects."
-                    >
-                      Introduction to Software Testing
-                      <span class="badge bg-primary rounded-pill">
-                        IN PROGRESS
-                      </span>
-                    </li>
-                    <li
-                      class="list-group-item d-flex justify-content-between align-items-center"
-                      data-bs-toggle="tooltip"
-                      data-bs-placement="top"
-                      title="From the Iowa State Catalog: SE339 - Modeling and design of software at the architectural level. Architectural styles. Basics of model-driven architecture. Object-oriented design and analysis. Iterative development and unified process. Design patterns. Design by contract. Component based design. Product families. Measurement theory and appropriate use of metrics in design. Designing for qualities such as performance, safety, security, reliability, reusability, etc. Analysis and evaluation of software architectures. Introduction to architecture definition languages. Basics of software evolution, reengineering, and reverse engineering. Case studies. Introduction to distributed system software."
-                    >
-                      Software Architecture and Design
-                      <span class="badge bg-primary rounded-pill">
-                        IN PROGRESS
-                      </span>
-                    </li>
+                  <ul>
+                    {educationItems.map((education) => (
+                      <Accordion key={education.id}>
+                        <AccordionSummary
+                          expandIcon={<ExpandMoreIcon />}
+                          aria-controls="panel1a-content"
+                          id={education.id}
+                        >
+                          <Stack
+                            direction="row"
+                            justifyContent="space-evenly"
+                            alignItems="center"
+                            spacing={2}
+                          >
+                            <Typography variant="text">
+                              {education.title}
+                            </Typography>
+                          </Stack>
+                        </AccordionSummary>
+                        <AccordionDetails sx={{ textAlign: "center" }}>
+                          <Typography sx={{ marginBottom: "20px" }}>
+                            My grade:{" "}
+                            <span className="badge bg-primary rounded-pill">
+                              {education.grade}
+                            </span>
+                          </Typography>
+                          <Typography>{education.description}</Typography>
+                        </AccordionDetails>
+                      </Accordion>
+                    ))}
                   </ul>
                 </TextWrapper>
               </Column1>

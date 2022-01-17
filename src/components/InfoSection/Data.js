52f26412a63a9aa450a8826575cbe54c05743e1d
ajmazzie <ajmazzie@iastate.edu>
2022-01-16 21:21:51 -0600

    spelling

--- src/components/InfoSection/Data.js
+++ src/components/InfoSection/Data.js
@@ -6,7 +6,7 @@ export const homeObjOne = {
   topLine: "About Me",
   headLine: "Father, Veteran, Engineer",
   description:
-    'I am a father of two, a United States Army Veteran, and a Software Engineering Junior at Iowa State University. For as long as I can remember, I have been a technology enthusiast. Some of my fondest memories from childhood are learning how to navigate the file structure, changing settings, and reformatting the operating system when I occasionally broke something. Spring 2020 I began Software Engineering school at Iowa State University. Aside from school I have enjoyed constructing small programs like "Bop-It!", "Word Fall", and "Snake" using languages like C, Java, and JavaScript. Most recently I have been ReactJS applications and hosting them using Amazon Web Services. I am enthusiastic to continue to learn and grow!',
+    'I am a father of two, a United States Army Veteran, and a Software Engineering Junior at Iowa State University. For as long as I can remember, I have been a technology enthusiast. Some of my fondest memories from childhood are learning how to navigate the file structure, changing settings, and reformatting the operating system when I occasionally broke something. Spring 2020 I began Software Engineering school at Iowa State University. Aside from school I have enjoyed constructing small programs like "Bop-It!", "Word Fall", and "Snake" using languages like C, Java, and JavaScript. Most recently I have been developing ReactJS web applications and hosting them using Amazon Web Services. I am enthusiastic to continue to learn and grow!',
   buttonLabel: "See my work",
   to: "projects",
   imgStart: true,

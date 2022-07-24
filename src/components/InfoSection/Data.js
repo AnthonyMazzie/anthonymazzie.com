be49e5f9256f8b1bc64d79db850e319b541831a7
ajmazzie <ajmazzie@iastate.edu>
2022-07-24 00:33:38 -0500

    fix

--- src/components/InfoSection/Data.js
+++ src/components/InfoSection/Data.js
@@ -6,7 +6,7 @@ export const homeObjOne = {
   topLine: "About Me",
   headLine: "Father, Veteran, Engineer",
   description:
-    "I am a father of two, a United States Army Veteran, and a Software Engineering Senior at Iowa State University. For as long as I can remember, I have been a technology enthusiast. Some of my fondest memories from childhood are learning how to navigate the file structure, changing settings, and reformatting the operating system when I occasionally broke something. Spring 2020 I began Software Engineering school at Iowa State University. I anticipate graduating with a Bachelor of Engineering in May 2023. I am a lifelong learner who is passionate and enthusiastic about technology.",
+    "I am a father of two, a United States Army Veteran, and a Software Engineering Senior at Iowa State University. For as long as I can remember, I have been a technology enthusiast. Some of my fondest memories from childhood are learning how to navigate the file structure, changing settings, and reformatting the operating system when I occasionally broke something. Spring 2020 I began Software Engineering school at Iowa State University. I anticipate graduating with a Bachelor of Engineering in May 2023.",
   buttonLabel: "See my work",
   to: "projects",
   imgStart: true,

893b21496abe1a9645f1f3bcc7d986a218b517c3
ajmazzie <ajmazzie@iastate.edu>
2022-07-24 00:19:38 -0500

    minor

--- src/components/ProjectsSection/Data.js
+++ src/components/ProjectsSection/Data.js
@@ -4,10 +4,7 @@ export const projectsObj = {
   lightText: false,
   lightTextDesc: false,
   headLine: "Because learning by doing is the best way",
-  description1:
-    "I began formally studying Software Engineering in the Spring of 2020 at Iowa State University. I will graduate with a Bachelor of Engineering in Software Engineering after Spring 2023.",
-  description2:
-    "The classes I have taken so far at Iowa State University that relate to software are:",
+  description1: "Software classes I have taken at Iowa State University:",
   imgStart: false,
   img: require("../../images/programming.svg").default,
   alt: "Car",
